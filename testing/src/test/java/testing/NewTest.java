76987fc518d0d31965f9fb1ad162ccc85468b55a
ashishAppirio <ashish.jain@appirio.com>
2020-05-20 11:47:43 +0530

    commit 2

--- testing/src/test/java/testing/NewTest.java
+++ testing/src/test/java/testing/NewTest.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("hello world");
+	  System.out.println("Hello World!! Again !!");
   }
 }
